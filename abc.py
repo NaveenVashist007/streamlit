ea5e7332d10a9cc6fbf2a18ca35b99735b4ccc81
NaveenVashist007 <nk20498@gmail.com>
2024-05-21 12:14:18 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title('Hello')
+n=st.text_input('Enter your name')
+if n=='python':
+    st.write('Welcome to cetpa')
+x=st.number_input('Enter your marks')
+if x>=75
+    st.write('you are eligible for 75% discount')
+    
