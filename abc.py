683edf2be323e28aae21ebd8b09147197099575d
NaveenVashist007 <nk20498@gmail.com>
2024-05-21 12:23:02 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -4,6 +4,6 @@ n=st.text_input('Enter your name')
 if n=='python':
     st.write('Welcome to cetpa')
 x=st.number_input('Enter your marks')
-if x>=75
+if x>=75:
     st.write('you are eligible for 75% discount')
     
