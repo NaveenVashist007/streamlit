d021ac9aed85f430fd954931b1e7d0a54b01c021
NaveenVashist007 <nk20498@gmail.com>
2024-05-28 17:17:15 +0530

    Add files via upload

--- abc.py
+++ abc.py
@@ -1,9 +1,30 @@
 import streamlit as st
-st.title('Hello')
-n=st.text_input('Enter your name')
-if n=='python':
-    st.write('Welcome to cetpa')
-x=st.number_input('Enter your marks')
-if x>=75:
-    st.write('you are eligible for 75% discount')
-    
+st.title('Welcome Learner')
+n=st.text_input('Enter your name:')
+if n=='Naveen':
+    st.balloons()
+    st.write(n,'Welcome to Cetpa')
+x=st.number_input('Enter your age')
+if x>=10 and x<=18:
+    st.write('You are eligible for online course')
+elif x>18 and x<=21:
+    st.write('You are eligible for week end course and online course')
+elif x>21 and x<=40:
+    st.write('You are eligible for job oriented regular course')
+a=st.radio('Are you working?',options=('yes','no'))
+if a=='yes':
+    st.write('You are eligible for week end course and online course')
+else:
+    st.write('You are eligible for job oriented regular course')
+s=st.sidebar.checkbox('Are you interested in programming')
+if s==True:
+    t=st.sidebar.radio('You can choose language',options=('Python','Java','c'))
+    if t==('Python'):
+        st.sidebar.write('Welcome to python')
+else:
+    st.sidebar.write('You can enerol for sql')
+
+
+
+
+
